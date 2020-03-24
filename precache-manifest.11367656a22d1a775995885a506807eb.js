a9d4ed84bd6be132ac14ddd9bc0452338d07a45f
Steve Alden <stevealden.77@gmail.com>
2020-03-24 16:15:53 +0530

    Updates

--- precache-manifest.11367656a22d1a775995885a506807eb.js
+++ precache-manifest.11367656a22d1a775995885a506807eb.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "69b31d6efeb4b6a394875a8e96e5f3db",
+    "revision": "867aa5eae94e84353dc53b486365b563",
     "url": "/react-pokedex-carousel/index.html"
   },
   {
-    "revision": "5fecccb23ca18e07e6eb",
+    "revision": "b0e3f5a43063ef1f0cb2",
     "url": "/react-pokedex-carousel/static/css/main.d1b05096.chunk.css"
   },
   {
-    "revision": "5534556edad0465829e5",
-    "url": "/react-pokedex-carousel/static/js/2.4c576391.chunk.js"
+    "revision": "8ef72b8b3412d1f006dd",
+    "url": "/react-pokedex-carousel/static/js/2.614824ab.chunk.js"
   },
   {
     "revision": "12dadb27089f85a033eb26c4f03331ef",
-    "url": "/react-pokedex-carousel/static/js/2.4c576391.chunk.js.LICENSE.txt"
+    "url": "/react-pokedex-carousel/static/js/2.614824ab.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "5fecccb23ca18e07e6eb",
-    "url": "/react-pokedex-carousel/static/js/main.c382ef73.chunk.js"
+    "revision": "b0e3f5a43063ef1f0cb2",
+    "url": "/react-pokedex-carousel/static/js/main.f3364862.chunk.js"
   },
   {
     "revision": "744e5f3f07f6e16b1d7c",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-pokedex-carousel/precache-manifest.7c2fe694036931a7d36a2a7059728ff6.js"
+  "/react-pokedex-carousel/precache-manifest.11367656a22d1a775995885a506807eb.js"
 );
 
 self.addEventListener('message', (event) => {
