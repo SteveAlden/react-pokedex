ecd4a57105c75c9692e309464a5358cfde1de5e6
Steve Alden <stevealden.77@gmail.com>
2020-03-30 18:11:34 +0530

    fix

--- src/components/pokemon/PokeList.js
+++ src/components/pokemon/PokeList.js
@@ -20,15 +20,15 @@ class PokeList extends Component {
       <>
         <StackGrid
           columnWidth={190}
-          // appear={scaleDown.appear}
-          // appeared={scaleDown.appeared}
-          // enter={scaleDown.enter}
-          // entered={scaleDown.entered}
-          // leaved={scaleDown.leaved}
+          appear={scaleDown.appear}
+          appeared={scaleDown.appeared}
+          enter={scaleDown.enter}
+          entered={scaleDown.entered}
+          leaved={scaleDown.leaved}
           gutterWidth={15}
           gutterHeight={15}
-          // appearDelay={80}
-          // duration={200}
+          appearDelay={80}
+          duration={200}
         >
           {pokemon?.map(p => (
             <Pokemon poke={p} />
