467ba02cbbf1482033d5ae715b82c4cbbc74fe03
Steve Alden <stevealden.77@gmail.com>
2020-03-31 11:08:23 +0530

    Separated chip

---
+++ src/components/helpers/Chip.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { Chip } from '@material-ui/core';
+
+const Chips = props => {
+  return (
+    <Chip
+      size={props.size}
+      label={props.label}
+      color='primary'
+      style={{ backgroundColor: getChipStyle(props.label) }}
+    />
+  );
+};
+
+const getChipStyle = pokeType => {
+  switch (pokeType) {
+    case 'Bug':
+      return '#C3D21F';
+    case 'Dark':
+      return '#8E6956';
+    case 'Dragon':
+      return '#8774FF';
+    case 'Electric':
+      return '#FDE53D';
+    case 'Fairy':
+      return '#F9ADFF';
+    case 'Fighting':
+      return '#A85643';
+    case 'Fire':
+      return '#FA5643';
+    case 'Flying':
+      return '#79A4FF';
+    case 'Ghost':
+      return '#7873D4';
+    case 'Grass':
+      return '#8DD851';
+    case 'Ground':
+      return '#EDCC56';
+    case 'Ice':
+      return '#96F1FF';
+    case 'Normal':
+      return '#BDBDAE';
+    case 'Poison':
+      return '#AB5DA2';
+    case 'Psychic':
+      return '#F662B1';
+    case 'Rock':
+      return '#CDBD72';
+    case 'Stell':
+      return '#C4C2DB';
+    case 'Water':
+      return '#56AEFF';
+  }
+};
+
+export default Chips;
--- src/components/pokemon/DisplayPokemon.js
+++ src/components/pokemon/DisplayPokemon.js
@@ -2,10 +2,10 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import { Container, Row, Col } from 'react-bootstrap';
 import Evolutions from '../helpers/Evolutions';
-import { Chip } from '@material-ui/core';
 import Stats from '../helpers/Stats';
 import { NavArrowLeft, NavArrowRight } from '../helpers/NavArrows';
 import NotFoundPage from '../../components/NotFound';
+import Chips from '../helpers/Chip';
 
 class DisplayPokemon extends Component {
   constructor(props) {
@@ -71,47 +71,6 @@ class DisplayPokemon extends Component {
   componentDidMount() {
     this.updateComponent(this.props.match.params.id);
   }
-
-  getChipStyle = pokeType => {
-    switch (pokeType) {
-      case 'Bug':
-        return '#C3D21F';
-      case 'Dark':
-        return '#8E6956';
-      case 'Dragon':
-        return '#8774FF';
-      case 'Electric':
-        return '#FDE53D';
-      case 'Fairy':
-        return '#F9ADFF';
-      case 'Fighting':
-        return '#A85643';
-      case 'Fire':
-        return '#FA5643';
-      case 'Flying':
-        return '#79A4FF';
-      case 'Ghost':
-        return '#7873D4';
-      case 'Grass':
-        return '#8DD851';
-      case 'Ground':
-        return '#EDCC56';
-      case 'Ice':
-        return '#96F1FF';
-      case 'Normal':
-        return '#BDBDAE';
-      case 'Poison':
-        return '#AB5DA2';
-      case 'Psychic':
-        return '#F662B1';
-      case 'Rock':
-        return '#CDBD72';
-      case 'Stell':
-        return '#C4C2DB';
-      case 'Water':
-        return '#56AEFF';
-    }
-  };
 
   createRowCol = (col1, col2) => {
     return (
@@ -272,12 +231,7 @@ class DisplayPokemon extends Component {
           <div>
             {pokemonDisplay?.type?.map(t => (
               <>
-                <Chip
-                  // size='small'
-                  label={t}
-                  color='primary'
-                  style={{ backgroundColor: this.getChipStyle(t) }}
-                />{' '}
+                <Chips size='medium' label={t} />{' '}
               </>
             ))}
           </div>
@@ -304,12 +258,7 @@ class DisplayPokemon extends Component {
             <h3>Weakness</h3>
             {pokemonDisplay?.weaknesses?.map(t => (
               <>
-                <Chip
-                  size='small'
-                  label={t}
-                  color='primary'
-                  style={{ backgroundColor: this.getChipStyle(t) }}
-                />{' '}
+                <Chips size='small' label={t} />{' '}
               </>
             ))}
           </div>
