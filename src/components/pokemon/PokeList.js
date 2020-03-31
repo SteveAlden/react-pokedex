6c1f0f6bcd3b3485a20764918f72439b543074fc
Steve Alden <stevealden.77@gmail.com>
2020-03-31 10:42:10 +0530

    Made pokelist a function component

--- src/components/pokemon/PokeList.js
+++ src/components/pokemon/PokeList.js
@@ -1,42 +1,32 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Pokemon from './Pokemon';
 import StackGrid, { transitions } from 'react-stack-grid';
 const { scaleDown } = transitions;
 
-class PokeList extends Component {
-  state = {
-    displayNo: 1
-  };
-
-  render() {
-    let pokemon;
-    if (this.props?.pokemons) {
-      pokemon = this.props.pokemons;
-      console.log('Pokelist');
-      console.log(pokemon);
-    }
-
-    return (
-      <>
-        <StackGrid
-          columnWidth={190}
-          appear={scaleDown.appear}
-          appeared={scaleDown.appeared}
-          enter={scaleDown.enter}
-          entered={scaleDown.entered}
-          leaved={scaleDown.leaved}
-          gutterWidth={15}
-          gutterHeight={15}
-          appearDelay={80}
-          duration={200}
-        >
-          {pokemon?.map(p => (
-            <Pokemon poke={p} />
-          ))}
-        </StackGrid>
-      </>
-    );
+const PokeList = props => {
+  let pokemon;
+  if (props?.pokemons) {
+    pokemon = props.pokemons;
   }
-}
+
+  return (
+    <StackGrid
+      columnWidth={180}
+      appear={scaleDown.appear}
+      appeared={scaleDown.appeared}
+      enter={scaleDown.enter}
+      entered={scaleDown.entered}
+      leaved={scaleDown.leaved}
+      gutterWidth={15}
+      gutterHeight={15}
+      appearDelay={80}
+      duration={200}
+    >
+      {pokemon?.map(p => (
+        <Pokemon poke={p} />
+      ))}
+    </StackGrid>
+  );
+};
 
 export default PokeList;
