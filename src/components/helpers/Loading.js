045fc1a0bbc6c548f1735c8df0943b2f03a798c4
Steve Alden <stevealden.77@gmail.com>
2020-04-01 20:44:56 +0530

    Loading into function

--- src/components/helpers/Loading.js
+++ src/components/helpers/Loading.js
@@ -1,30 +1,17 @@
 import React from 'react';
 import ReactLoading from 'react-loading';
 import FadeIn from 'react-fade-in';
-export default class Loading extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      done: undefined
-    };
-  }
-  componentDidMount() {
-    setTimeout(() => {
-      this.setState({ done: true });
-    }, 3000);
-  }
-  render() {
-    return (
-      <div align='center' style={{ marginTop: '20%' }}>
-        <FadeIn>
-          <ReactLoading
-            // type={'bubbles'}
-            type={'bars'}
-            color={'white'}
-            //  width={'5%'}
-          />
-        </FadeIn>
-      </div>
-    );
-  }
-}
+const Loading = () => {
+  return (
+    <div align='center' style={{ marginTop: '20%' }}>
+      <FadeIn>
+        <ReactLoading
+          // type={'bubbles'}
+          type={'bars'}
+          color={'white'}
+        />
+      </FadeIn>
+    </div>
+  );
+};
+export default Loading;
