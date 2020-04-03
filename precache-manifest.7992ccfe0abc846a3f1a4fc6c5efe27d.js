12856f58c5746037c608aff96a8fe436fc6b0b8c
Steve Alden <stevealden.77@gmail.com>
2020-04-03 16:39:05 +0530

    Updates

---
+++ precache-manifest.7992ccfe0abc846a3f1a4fc6c5efe27d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59ea2972e035cd32abecf9956ce80893",
+    "url": "/react-pokedex/home/index.html"
+  },
+  {
+    "revision": "2d1b8b641790dd0a7669",
+    "url": "/react-pokedex/home/static/css/main.b18af190.chunk.css"
+  },
+  {
+    "revision": "b702c43b3898ef9b8dbb",
+    "url": "/react-pokedex/home/static/js/2.ce6f303f.chunk.js"
+  },
+  {
+    "revision": "f80530daf6eeebfd02620acce8be7fd9",
+    "url": "/react-pokedex/home/static/js/2.ce6f303f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d1b8b641790dd0a7669",
+    "url": "/react-pokedex/home/static/js/main.67bfb4e5.chunk.js"
+  },
+  {
+    "revision": "81defa37a1be8afaea3f",
+    "url": "/react-pokedex/home/static/js/runtime-main.4e60c3fb.js"
+  },
+  {
+    "revision": "f561c6cc1a67e89471db6243ab195c42",
+    "url": "/react-pokedex/home/static/media/Oxanium-Regular.f561c6cc.ttf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/home/precache-manifest.4fdf9a7d8429c0b0b990734c93f25651.js"
+  "/react-pokedex/home/precache-manifest.7992ccfe0abc846a3f1a4fc6c5efe27d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/home/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-pokedex/home/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
